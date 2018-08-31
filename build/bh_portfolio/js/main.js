3ca9ad9de0f02ee0b40737b36b1ca86e876407f6
Bryan Henson <b.henson915@gmail.com>
2018-08-30 20:42:09 -0500

    navigation and atomic design

--- build/bh_portfolio/js/main.js
+++ build/bh_portfolio/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     $('#btn-menu').click(function(){
         $(this).toggleClass('fancy');
-        $('.navigation').toggle('slide', function(){
+        $('.mobile-nav').toggle('slide', function(){
             if ($(this).is(':visible'))
             $(this).css('display','flex');
         });
-        $('.navigation-list').toggle('slide', function(){
+        $('.mobile-nav-list').toggle('slide', function(){
             if ($(this).is(':visible'))
-            $(this).css('display','flex');
+            $(this).css('display','block');
         });
         $('.fa-bars').toggleClass('fa-times', 1000, "easeOutSine");
     });
